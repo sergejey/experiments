4f65a8b05a3bd68a53bd85e6809b623b2213e3c4
Sergei Jeihala <jey@tut.by>
2016-11-30 18:03:18 +0300

    Image source update

--- ar/js/threex/threex.imagegrabbing.js
+++ ar/js/threex/threex.imagegrabbing.js
@@ -8,7 +8,11 @@ THREEx.ImageGrabbing = function(){
 
         var domElement  = document.createElement('img')
         // TODO make this url tunable
-        domElement.src  = './image.jpg'
+
+        var src=location.hash.replace('#', '');
+        //alert(src);
+        
+        domElement.src  = './'+src+'.jpg'
 
         domElement.style.zIndex = -1;
         domElement.style.position = 'absolute'
